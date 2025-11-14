3935bb57a74ed97451bce905acaf72f51264c8de
eaahl <eksahlijah@st.ug.edu.gh>
2025-11-14 22:20:12 +0000

    Fix output message for bonus calculation

--- yieldEstimator.py
+++ yieldEstimator.py
@@ -2,4 +2,4 @@ bags = int(input("How many bags were harvested: "))
 total = bags * 850
 print("The bags cost a total of GHS", total)
 if bags > 100:
-    print("You also get a bonus of GHS 2000, therefore your new total is", total+2000)
+    print("You also get a bonus of GHS 2000, therefore your new total is GHS", total+2000)
