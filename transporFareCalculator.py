6466b78a4440711c4aa10176f546d8d5b62a8112
eaahl <eksahlijah@st.ug.edu.gh>
2025-11-14 22:01:11 +0000

    Implement fare calculator for passenger routes

---
+++ transporFareCalculator.py
@@ -0,0 +1,12 @@
+passenger_route= input("Enter the passenger route: ")
+passengers= int(input("Enter the total number of passengers boarding the trotro: "))
+fare = 0
+if passenger_route =="madina":
+    fare = 5
+elif passenger_route =="kasoa":
+    fare = 10
+elif passenger_route =="Tema":
+    fare = 8
+print(f"Route: {passenger_route} \nFare: {fare} \nPassengers: {passengers}")
+totalFare = passengers * fare
+print("The total fare for the trip is",totalFare)
